7152fa725ddef78f545caf5ca66c0f420d6526a8
kauafcamidu <168488826+kauafcamidu@users.noreply.github.com>
2024-10-31 20:09:25 -0300

    Add files via upload

---
+++ exercicio10/exercicio10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            retangulo ret;
+            ret = new retangulo();
+
+            Console.Write("Digite a Base do retangulo:");
+            ret.setBase(int.Parse(Console.ReadLine()));
+
+            Console.Write("Digite a altura do retangulo:");
+            ret.setAltura(int.Parse(Console.ReadLine()));
+
+            ret.calcularArea();
+
+            Console.WriteLine("Área do retângulo: {0}", ret.getArea());
+
+            if (ret.getArea() > 100)
+                Console.WriteLine("Terreno Grande");
+            else
+                Console.WriteLine("Terreno Pequeno");
+
+
+
+
+        }
+    }
+}
---
+++ exercicio10/exercicio10/retangulo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio10
+{
+    internal class retangulo
+    {
+        private int b;
+        private int h;
+        private int area;
+
+        public void setBase(int b)
+        {
+            this.b = b;
+        }
+        public int getBase()
+        {
+            return this.b;
+        }
+        public void setAltura(int h)
+        {
+            this.h = h;
+        }
+        public int getAltura()
+        {
+            return this.h;
+        }
+        public int getArea()
+        {
+            return this.area;
+        }
+        public void calcularArea()
+        {
+            this.area = this.b * this.h;
+        }
+
+
+    }
+}
