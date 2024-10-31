987010d2de931d1639640326183af05dc257fd8b
kauafcamidu <168488826+kauafcamidu@users.noreply.github.com>
2024-10-31 20:04:45 -0300

    Add files via upload

---
+++ exercicio2/Aresta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio3
+{
+    internal class Aresta
+    {
+        private int a;
+        private int area;
+
+        public void SetAresta(int a)
+        {
+            this.a = a;
+
+        }
+
+        public int GetAresta()
+        {
+            return this.a;
+        }
+       public int GetArea()
+       {
+            return this.area;
+       }
+
+        public void calcularArea()
+        {
+            this.area = this.a * this.a;
+        }
+    }
+}
---
+++ exercicio2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aresta are;
+            are = new Aresta();
+
+            Console.Write("Informe o valor da aresta do quadrdo");
+            are.SetAresta(int.Parse(Console.ReadLine()));
+
+           are.calcularArea();
+            Console.WriteLine("Área do quadrado: {0}", are.GetArea());
+
+        }
+    }
+}
