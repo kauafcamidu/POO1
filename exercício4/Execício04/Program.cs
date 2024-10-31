f54eb9ad7e3acb1cd1acc435d52d8038095d83d2
kauafcamidu <168488826+kauafcamidu@users.noreply.github.com>
2024-10-31 20:05:31 -0300

    Add files via upload

---
+++ exercício4/Execício04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execício04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TRIANGULO tri;
+            tri = new TRIANGULO();
+
+            Console.Write("Insira o valor da base do Triângulo:");
+            tri.SetBase(int.Parse(Console.ReadLine()));
+
+            Console.Write("Insira o valor da Altura do Triângulo:");
+            tri.SetAltura(int.Parse(Console.ReadLine()));
+
+            tri.CalcularArea();
+            Console.WriteLine("Área do Triângulo é: {0}", tri.GetArea());
+
+
+
+
+
+        }
+    }
+}
---
+++ exercício4/Execício04/TRIANGULO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execício04
+{
+    internal class TRIANGULO
+    {
+        private int b;
+        private int h;
+        private int area;
+
+
+        public void SetBase(int b)
+        {
+            this.b = b;
+        }
+
+        public void SetAltura(int h)
+        {
+            this.h = h;
+        }
+
+        public int GetBase()
+        {
+            return this.b;
+        }
+
+        public int GetAltura()
+        {
+            return this.h;
+        }
+
+        public int GetArea()
+        {
+            return this.area;
+        }
+
+        public void CalcularArea()
+        {
+            this.area = this.b * this.h / 2;
+        }
+    }
+}
