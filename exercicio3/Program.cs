04bf5d140dc00e371c371bb0ac2327cc0d7fe0c7
kauafcamidu <168488826+kauafcamidu@users.noreply.github.com>
2024-10-31 20:05:06 -0300

    Add files via upload

---
+++ exercicio3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado qua;
+            qua = new Quadrado();
+
+            Console.Write("Informe o valor da diagonaldo quadrado");
+            qua.SetDiagonal(int.Parse(Console.ReadLine()));
+
+            qua.calcularArea();
+
+            Console.WriteLine("Área do Quadrado:{0}", qua.GetArea());
+        }
+    }
+}
---
+++ exercicio3/Quadrado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    internal class Quadrado
+    {
+        private int d;
+        private int area;
+
+        public void SetDiagonal(int d)
+        {
+            this.d = d;
+        }
+        public int GetDiagonal() 
+        {
+            return this.d;
+
+        }
+        public int GetArea ()
+        {
+            return this.area;
+        }
+        
+        public void calcularArea()
+        {
+            this.area = this.d * this.d / 2;
+        }
+    }
+}
