266fd1a5e1298273c96d38db3cccfdb3ca0f73e3
kauafcamidu <168488826+kauafcamidu@users.noreply.github.com>
2024-10-31 20:07:16 -0300

    Add files via upload

---
+++ exercicio6/Milha.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio6
+{
+    internal class Milha
+    {
+        private double milhasMaritimas;
+        private double Quilometros;
+
+        public void SetMilhasMaritimas(double milhasMaritimas)
+        {
+            this.milhasMaritimas = milhasMaritimas;
+            
+        }
+        public double GetMilhasMaritimas()
+        {
+            return this.milhasMaritimas;
+        }
+        public double GetQuilometros()
+        {
+            return this.Quilometros;
+        }
+        public void CalcularQuilometros()
+        {
+            this.Quilometros = this.milhasMaritimas * 1.852;
+        }
+    }
+}
---
+++ exercicio6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Milha mil;
+            mil = new Milha();
+
+            Console.Write("Digite o número de milhas marítimas:");
+            mil.SetMilhasMaritimas(double.Parse(Console.ReadLine()));
+
+            mil.CalcularQuilometros();
+            
+            Console.WriteLine("Milhas Marítimas: " + mil.GetMilhasMaritimas());
+            Console.WriteLine("Quilômetros:" + mil.GetQuilometros());
+            
+
+
+
+
+
+        }
+    }
+}
